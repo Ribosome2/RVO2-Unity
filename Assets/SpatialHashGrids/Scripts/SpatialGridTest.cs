f9a8619dbdaa334edd97ff1381137f77338e9c7a
Ribosome2 <583154233@qq.com>
2024-08-01 15:22:23 +0800

    提交一版性能还不如直接搜索的spatialGrid

---
+++ Assets/SpatialHashGrids/Scripts/SpatialGridTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace SpatialGrids
+{
+    public class SpatialGridTest:MonoBehaviour
+    {
+        public Bounds gridBounds=new Bounds(Vector3.zero,new Vector3(100,100,1));
+        public int rows =100;
+        public int col =100;
+
+        public SpatialHashGrid testGrid;
+        public int moveClientNum = 60;
+        public GameObject PlayerGo;
+        public Vector3 PlayerSize=new Vector3(3,3,1);
+
+        class  TestUnit
+        {
+            public GameObject go;
+            public GridClient gridClient;
+        }
+
+        private List<TestUnit> _testUnits = new List<TestUnit>();
+        public GameObject moveClientPrefab;
+        public bool FindNearEveryFrame = true;
+        public bool FindWithForce = true; //暴力查找的方式
+        private void Start()
+        {
+            testGrid=new SpatialHashGrid(gridBounds,new Vector2(rows,col));
+            for (int i = 0; i < moveClientNum; i++)
+            {
+                var go = Instantiate(moveClientPrefab);
+                go.transform.position = new Vector3(Random.Range(gridBounds.min.x, gridBounds.max.x),
+                    Random.Range(gridBounds.min.y, gridBounds.max.y));
+
+                GridClient client = testGrid.NewClient(go.transform.position, new Vector2(1, 1));
+                _testUnits.Add(new TestUnit()
+                {
+                    go=go,
+                    gridClient = client
+                });
+               
+            }
+        }
+
+        private void Update()
+        {
+            if (FindNearEveryFrame)
+            {
+                FindNearTest();
+            }
+        }
+
+        [ContextMenu("FindNear")]
+        void FindNearTest()
+        {
+            if (PlayerGo == null)
+            {
+                return;
+            }
+            var pos = PlayerGo.transform.position;
+            var searchBounds = new Bounds(pos, PlayerSize);
+            if (FindWithForce)
+            {
+                result = new HashSet<GridClient>();
+                foreach (var testUnit in _testUnits)
+                {
+                    if (searchBounds.Contains(testUnit.gridClient.position))
+                    {
+                        result.Add(testUnit.gridClient);
+                    }
+                }
+            }
+            else
+            {
+                
+                result=testGrid.FindNear(pos, searchBounds);
+            }
+         
+            // Debug.Log("findCount "+ result.Count);
+        }
+
+        private HashSet<GridClient> result = new HashSet<GridClient>();
+        private void OnDrawGizmos()
+        {
+            var prevColor = Gizmos.color;
+            Gizmos.color=Color.cyan;
+            Gizmos.DrawWireCube(PlayerGo.transform.position,PlayerSize);
+            foreach (var gridClient in result)
+            {
+                Gizmos.DrawWireCube(gridClient.position,new Vector3(gridClient.dimensions.x,gridClient.dimensions.y,1));
+            }
+
+            Gizmos.color = prevColor;
+        }
+    }
+}
---
+++ Assets/SpatialHashGrids/Scripts/SpatialHashGrid.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpatialGrids
+{
+	
+
+	public class GridClient
+	{
+		public Vector2 position;   
+		public Vector2 dimensions; //width and height
+		public int[] gridRange=new int[4];  //占的格子索引范围，0，1是x的区间，2 3是y的区间
+	}
+
+	public class SpatialHashGrid
+	{
+		public Bounds Bounds;
+		public Vector2 Dimensions; //划分为几行几列
+		public Dictionary<string, List<GridClient>> cells=new Dictionary<string, List<GridClient>>();
+
+		public SpatialHashGrid(Bounds bounds, Vector2 dimensions)
+		{
+			this.Bounds = bounds;
+			this.Dimensions = dimensions;
+		}
+
+		public GridClient NewClient(Vector2 position, Vector2 dimensions)
+		{
+			var client = new GridClient()
+			{
+				position = position,
+				dimensions = dimensions,
+			};
+
+			this._Insert(client);
+			
+			return client;
+		}
+
+		private void _Insert(GridClient client)
+		{
+			float x = client.position.x;
+			float y = client.position.y;
+
+			float w = client.dimensions.x;
+			float h = client.dimensions.y;
+			int minX;
+			int minY;
+			int maxX;
+			int maxY;
+			this._GetCellIndex(x - w / 2, y - h / 2,out  minX,out  minY ); //用Client左下角取到的做索引
+			this._GetCellIndex(x + w / 2, y + h / 2,out maxX,out maxY); //用Client右上角取到的索引
+
+			for (int xIndex = minX; xIndex < maxX; xIndex++)
+			{
+				for (int yIndex = minY; yIndex < maxY; yIndex++)
+				{
+					var key = this._Key(xIndex, yIndex);
+					if (cells.ContainsKey(key) == false)
+					{
+						cells[key] = new List<GridClient>();
+					}
+					this.cells[key].Add(client);
+				}
+			}
+
+			client.gridRange[0] = minX;
+			client.gridRange[1] = maxX;
+			client.gridRange[2] = maxY;
+			client.gridRange[3] = maxY;
+
+		}
+
+		private string _Key(int xIndex, int yIndex)
+		{
+			return xIndex + "." + yIndex;
+		}
+
+		private void _GetCellIndex(float x, float y,out int xIndex,out int yIndex)
+		{
+			var factorX = (x - this.Bounds.min.x) / this.Bounds.size.x;
+			xIndex = Mathf.FloorToInt(factorX * this.Dimensions.x);
+			
+			var factorY = (y - this.Bounds.min.y) / this.Bounds.size.y;
+			yIndex = Mathf.FloorToInt(factorY * this.Dimensions.y);
+		}
+
+		public HashSet<GridClient> FindNear(Vector2 position, Bounds bound)
+		{
+			float x = position.x;
+			float y = position.y;
+
+			float w = bound.size.x;
+			float h = bound.size.y;
+			int minX;
+			int minY;
+			int maxX;
+			int maxY;
+			this._GetCellIndex(x - w / 2, y - h / 2, out minX, out minY); //用Client左下角取到的做索引
+			this._GetCellIndex(x + w / 2, y + h / 2, out maxX, out maxY); //用Client右上角取到的索引
+			HashSet<GridClient> clients = new HashSet<GridClient>();
+
+			for (int xIndex = minX; xIndex < maxX; xIndex++)
+			{
+				for (int yIndex = minY; yIndex < maxY; yIndex++)
+				{
+					var key = this._Key(xIndex, yIndex);
+					if (cells.ContainsKey(key))
+					{
+						foreach (var gridClient in this.cells[key])
+						{
+							clients.Add(gridClient);
+						}
+					}
+				}
+			}
+
+			return clients;
+		}
+
+		public void UpdateClient(GridClient client)
+		{
+			this.RemoveClient(client);
+			this._Insert(client);
+		}
+
+		private void RemoveClient(GridClient client)
+		{
+			
+			for (int xIndex = client.gridRange[0]; xIndex < client.gridRange[1]; xIndex++)
+			{
+				for (int yIndex = client.gridRange[2]; yIndex < client.gridRange[3]; yIndex++)
+				{
+					var key = this._Key(xIndex, yIndex);
+					if (cells.ContainsKey(key))
+					{
+						cells[key].Remove(client);
+					}
+				}
+			}
+		}
+	}
+}
+
+
